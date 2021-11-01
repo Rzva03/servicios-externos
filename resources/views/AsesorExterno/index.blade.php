fb4e46626c63b146ddc0893c90cc7a4511911f69
17920368 <avictor.zrodriguez@gmail.com>
2021-11-01 14:02:01 -0600

    VIEWS

--- resources/views/AsesorExterno/index.blade.php
+++ resources/views/AsesorExterno/index.blade.php
@@ -1,46 +1,3 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <button onclick="location.href='{{ route('asesor-externo.create') }}'" class="btn btn-primary">Nuevo</button>
-        <br><br>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Telefono</th>
-                    <th scope="col">Acciones</th>
-                    {{-- <th scope="col">Eliminar</th> --}}
-{{-- </tr>
-            </thead>
-            <tbody>
-                @foreach ($asesoresExternos as $asesorExterno)
-                    <tr>
-                        <th scope="row">{{ $asesorExterno->idAsesorE }}</th>
-                        <td> {{ $asesorExterno->nombre }} </td>
-                        <td> {{ $asesorExterno->email }} </td>
-                        <td> {{ $asesorExterno->telefono }} </td>
-                        <td>
-                            <button
-                                onclick="location.href='{{ route('asesor-externo.edit', $asesorExterno->idAsesorE) }}'"
-                                class="btn btn-outline-primary">Modificar</button>
-                            {{-- </td> --}}
-{{-- <td> --}}
-{{-- <br><br>
-                            <form action="{{ route('asesor-externo.destroy', $asesorExterno->idAsesorE) }}" method="POST">
-                                @csrf
-                                @method('delete')
-                                <button type="submit" class="btn btn-outline-danger">Eliminar</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-@endsection --}}
 @extends('layouts.app')
 
 @section('content')
@@ -48,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-10 col-xs-12">
                 <div class="card">
-                    <div class="card-header">{{ __('Asesor Externo') }}</div>
+                    <div class="card-header">{{ __('ASESOR EXTERNO') }}</div>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -56,8 +13,6 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-
-                        {{-- {{ __('You are logged in!') }} --}}
                         <button onclick="location.href='{{ route('asesor-externo.create') }}'"
                             class="btn btn-primary">Nuevo</button>
                         <br><br>
@@ -69,7 +24,6 @@
                                     <th scope="col">Email</th>
                                     <th scope="col">Telefono</th>
                                     <th scope="col">Acciones</th>
-                                    {{-- <th scope="col">Eliminar</th> --}}
                                 </tr>
                             </thead>
                             <tbody>
@@ -80,19 +34,19 @@
                                         <td> {{ $asesorExterno->email }} </td>
                                         <td> {{ $asesorExterno->telefono }} </td>
                                         <td>
-                                            <button
-                                                onclick="location.href='{{ route('asesor-externo.edit', $asesorExterno->idAsesorE) }}'"
-                                                class="btn btn-outline-primary">Modificar</button>
-                                            {{-- </td> --}}
-                                            {{-- <td> --}}
-                                            <br><br>
-                                            <form
-                                                action="{{ route('asesor-externo.destroy', $asesorExterno->idAsesorE) }}"
-                                                method="POST">
-                                                @csrf
-                                                @method('delete')
-                                                <button type="submit" class="btn btn-outline-danger">Eliminar</button>
-                                            </form>
+                                            <div style="display: flex; justify-content: space-evenly;">
+                                                <button style="margin-right: 1rem"
+                                                    onclick="location.href='{{ route('asesor-externo.edit', $asesorExterno->idAsesorE) }}'"
+                                                    class="btn btn-outline-primary">Modificar</button>
+                                                <br><br>
+                                                <form
+                                                    action="{{ route('asesor-externo.destroy', $asesorExterno->idAsesorE) }}"
+                                                    method="POST">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <button type="submit" class="btn btn-outline-danger">Eliminar</button>
+                                                </form>
+                                            </div>
                                         </td>
                                     </tr>
                                 @endforeach
